63cec2a665479190a1b398da979065ac930ee4ce
Your Name <you@example.com>
2025-05-15 15:36:53 -0300

    feat(core): criar entidade Usuario imutável com atributos e getters

--- src/main/java/com/pdfocus/core/models/Usuario.java
+++ src/main/java/com/pdfocus/core/models/Usuario.java
@@ -1,4 +1,35 @@
 package com.pdfocus.core.models;
 
+
+import java.util.UUID;
+
 public class Usuario {
+    private final UUID id;
+    private final String nome;
+    private final String email;
+    private final String senhaHash;
+
+    public Usuario( UUID id, String nome, String email, String senhaHash) {
+        this.id = id ;
+        this.nome = nome;
+        this.email = email;
+        this.senhaHash = senhaHash;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenhaHash() {
+        return senhaHash;
+    }
+
 }
