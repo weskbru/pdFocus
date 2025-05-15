d5df642881c2387e7af08aa9cbb865938ea938b8
Your Name <you@example.com>
2025-05-15 15:11:29 -0300

    chore: remove PdfocusApplicationTest do projeto

--- src/main/java/com/pdfocus/PdfocusApplication.java
+++ src/main/java/com/pdfocus/PdfocusApplication.java
@@ -4,7 +4,5 @@ public class PdfocusApplication {
     public static void main(String[] args) {
         System.out.println("Bora PORRAAAA!");
     }
-    public static int somar(int a, int b) {
-        return a + b;
-    }
+
 }
